cf34de9388774a025ef750eaef9a51cd7e0ba337
Delagardi <delagardi@redy.io>
2018-03-30 03:44:04 +0300

    added app.js and module.parent

--- server video 5/index.js
+++ server video 5/index.js
@@ -1,6 +1,14 @@
 var user = require ("./user");
 
-var vasya = new user.User("Вася");
-var petya = new user.User("Петя");
+function run() {
+  var vasya = new user.User("Вася");
+  var petya = new user.User("Петя");
 
-vasya.hello(petya);
+  vasya.hello(petya);
+}
+
+if (module.parent) {
+  exports.run = run;
+} else {
+  run();
+}
